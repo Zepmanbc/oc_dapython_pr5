db0ee77f4fba551ec1ae29651e50f142969ee9a7
ZepmanBC <zepman@gmail.com>
2019-02-07 20:58:58 +0100

    add ON DELETE CASCADE

--- app/static/dboff.sql
+++ app/static/dboff.sql
@@ -29,7 +29,7 @@ CREATE TABLE OffData(
     INDEX ind_product_id(product_id)
 );
 
-CREATE TABLE favorite(
+CREATE TABLE Substitute(
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
     origin_id INT UNSIGNED NOT NULL,
     substitute_id INT UNSIGNED NOT NULL,
@@ -39,12 +39,12 @@ CREATE TABLE favorite(
 );
 
 ALTER TABLE Product 
-ADD CONSTRAINT fk_category_id FOREIGN KEY (category_id) REFERENCES Category(id);
+ADD CONSTRAINT fk_category_id FOREIGN KEY (category_id) REFERENCES Category(id) ON DELETE CASCADE;
 
 ALTER TABLE OffData 
-ADD CONSTRAINT fk_product_id FOREIGN KEY (product_id) REFERENCES Product(id);
+ADD CONSTRAINT fk_product_id FOREIGN KEY (product_id) REFERENCES Product(id) ON DELETE CASCADE;
 
-ALTER TABLE favorite
-ADD CONSTRAINT fk_origin_id FOREIGN KEY (origin_id) REFERENCES OffData(id);
-ALTER TABLE favorite
-ADD CONSTRAINT fk_substitute_id FOREIGN KEY (substitute_id) REFERENCES OffData(id);
+ALTER TABLE Substitute
+ADD CONSTRAINT fk_origin_id FOREIGN KEY (origin_id) REFERENCES OffData(id) ON DELETE CASCADE;
+ALTER TABLE Substitute
+ADD CONSTRAINT fk_substitute_id FOREIGN KEY (substitute_id) REFERENCES OffData(id) ON DELETE CASCADE;
